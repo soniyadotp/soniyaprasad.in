1567fb28b75ccd98e6dd31d80b3fe3a4469b7928
Your Name <soniyagupta82270@gmail.com>
2024-10-27 19:37:07 +0530

    add projects

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,53 +1,60 @@
 import { renderContent } from "@/app/resources";
-import { Flex, IconButton, SmartLink, Text } from "@/once-ui/components"
+import { Flex, IconButton, SmartLink, Text } from "@/once-ui/components";
 import { useTranslations } from "next-intl";
 // import { person, social } from '@/app/resources'
 
 export const Footer = () => {
-    const currentYear = new Date().getFullYear();
+  const currentYear = new Date().getFullYear();
 
-    const t = useTranslations();
-    const { person, social } = renderContent(t);
+  const t = useTranslations();
+  const { person, social } = renderContent(t);
 
-    return (
-        <Flex
-            as="footer"
-            position="relative"
-            fillWidth padding="8"
-            justifyContent="center">
-            <Flex
-                fillWidth maxWidth="m" paddingY="8" paddingX="16"
-                justifyContent="space-between" alignItems="center">
-                <Text
-                    variant="body-default-s"
-                    onBackground="neutral-strong">
-                    <Text
-                        onBackground="neutral-weak">
-                        © {currentYear} /
-                    </Text>
-                    <Text paddingX="4">
-                        {person.name}
-                    </Text>
-                    <Text onBackground="neutral-weak">
-                        {/* Usage of this template requires attribution. Please don't remove the link to Once UI. */}
-                        / Build your portfolio with <SmartLink style={{marginLeft: '-0.125rem'}} href="https://once-ui.com/templates/magic-portfolio">Once UI</SmartLink>
-                    </Text>
-                </Text>
-                <Flex
-                    gap="16">
-                    {social.map((item) => (
-                        item.link && (
-                            <IconButton
-                                key={item.name}
-                                href={item.link}
-                                icon={item.icon}
-                                tooltip={item.name}
-                                size="s"
-                                variant="ghost"/>
-                        )
-                    ))}
-                </Flex>
-            </Flex>
+  return (
+    <Flex
+      as="footer"
+      position="relative"
+      fillWidth
+      padding="8"
+      justifyContent="center"
+    >
+      <Flex
+        fillWidth
+        maxWidth="m"
+        paddingY="8"
+        paddingX="16"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Text variant="body-default-s" onBackground="neutral-strong">
+          <Text onBackground="neutral-weak">© {currentYear} /</Text>
+          <Text paddingX="4">{person.name}</Text>
+          <Text onBackground="neutral-weak">
+            {/* Usage of this template requires attribution. Please don't remove the link to Once UI. */}
+            / Find my projects at{" "}
+            <SmartLink
+              style={{ marginLeft: "-0.125rem" }}
+              href="https://github.com/soniyaprasad"
+            >
+              Github
+            </SmartLink>
+          </Text>
+        </Text>
+        <Flex gap="16">
+          {social.map(
+            (item) =>
+              item.link && (
+                <IconButton
+                  key={item.name}
+                  href={item.link}
+                  icon={item.icon}
+                  tooltip={item.name}
+                  size="s"
+                  variant="ghost"
+                />
+              )
+          )}
         </Flex>
-    )
-}
+      </Flex>
+    </Flex>
+  );
+};
